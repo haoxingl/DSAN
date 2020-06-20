24fd1e09a3b0958054f83255ad382c045226d4a5
starlork <mb85410@umac.mo>
2020-06-20 14:51:11 +0800

    update hvd

--- cifar10_hvd.py
+++ cifar10_hvd.py
@@ -11,24 +11,14 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "3, 4"
 
 from tensorflow.keras import datasets, layers, models
 from six.moves import cPickle
-# import matplotlib.pyplot as plt
 
 
 def load_batch(fpath, label_key='labels'):
-    """Internal utility for parsing CIFAR data.
-    Arguments:
-        fpath: path the file to parse.
-        label_key: key for label data in the retrieve
-            dictionary.
-    Returns:
-        A tuple `(data, labels)`.
-    """
     with open(fpath, 'rb') as f:
         if sys.version_info < (3,):
             d = cPickle.load(f)
         else:
             d = cPickle.load(f, encoding='bytes')
-            # decode utf8
             d_decoded = {}
             for k, v in d.items():
                 d_decoded[k.decode('utf8')] = v
@@ -52,7 +42,6 @@ if gpus:
 
 print(hvd.local_rank())
 
-# (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
 num_train_samples = 50000
 
 x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
@@ -63,23 +52,14 @@ for i in range(1, 6):
     (x_train[(i - 1) * 10000:i * 10000, :, :, :],
      y_train[(i - 1) * 10000:i * 10000]) = load_batch(fpath)
 
-# x_test = np.empty((10000, 3, 32, 32), dtype='uint8')
-# y_test = np.empty((10000,), dtype='uint8')
-
-# fpath = os.path.join('data/cifar10/', 'test_batch')
-# x_test[...], y_test[...] = load_batch(fpath)
-
 train_labels = np.reshape(y_train, (len(y_train), 1))
-
-# test_images = x_test.astype(x_train.dtype)
-# test_labels = y_test.astype(y_train.dtype)
 
 # Normalize pixel values to be between 0 and 1
 x_train = x_train.transpose(0, 2, 3, 1)
 train_images = x_train / 255.0
 
-train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels)).batch(2048 // hvd.size())
-train_dataset = train_dataset.shuffle(num_train_samples)
+train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
+train_dataset = train_dataset.shuffle(num_train_samples).batch(2048 // hvd.size())
 
 model = models.Sequential()
 model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
@@ -87,8 +67,10 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+model.add(layers.Dropout(0.25))
 model.add(layers.Flatten())
-model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(10))
 
 model.summary()
@@ -96,8 +78,9 @@ model.summary()
 loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
 opt = tf.keras.optimizers.Adam(learning_rate=0.001 * hvd.size())
 
-# checkpoint_dir = './checkpoints'
-# checkpoint = tf.train.Checkpoint(model=model, optimizer=opt)
+checkpoint_dir = 'checkpoints/cifar10'
+checkpoint = tf.train.Checkpoint(model=model, optimizer=opt)
+ckpt_manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=10)
 
 @tf.function
 def training_step(images, labels, first_batch):
@@ -133,5 +116,5 @@ for i in range(10000):
 
     # Horovod: save checkpoints only on worker 0 to prevent other workers from
     # corrupting it.
-    # if hvd.rank() == 0:
-    #     checkpoint.save(checkpoint_dir)
+    if hvd.rank() == 0:
+        ckpt_manager.save()
