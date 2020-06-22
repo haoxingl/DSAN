687add35d27359b41d9758b9eedac197922d1f98
starlork <mb85410@umac.mo>
2020-06-22 11:16:00 +0800

    update hvd

--- cifar10_hvd.py
+++ cifar10_hvd.py
@@ -1,38 +1,16 @@
-from __future__ import absolute_import, division, print_function
-
-import sys
 import os
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "2, 3"
+
 import numpy as np
 import tensorflow as tf
 import horovod.tensorflow as hvd
 
-os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"] = "3, 4"
-
-from tensorflow.keras import datasets, layers, models
-from six.moves import cPickle
-
-
-def load_batch(fpath, label_key='labels'):
-    with open(fpath, 'rb') as f:
-        if sys.version_info < (3,):
-            d = cPickle.load(f)
-        else:
-            d = cPickle.load(f, encoding='bytes')
-            d_decoded = {}
-            for k, v in d.items():
-                d_decoded[k.decode('utf8')] = v
-            d = d_decoded
-    data = d['data']
-    labels = d[label_key]
-
-    data = data.reshape(data.shape[0], 3, 32, 32)
-    return data, labels
-
-
 print(tf.__version__)
 
 hvd.init()
+
+print('hvd.local_rank(): ', hvd.local_rank(), 'hvd.size(): ', hvd.size())
 
 gpus = tf.config.list_physical_devices('GPU')
 for gpu in gpus:
@@ -40,59 +18,46 @@ for gpu in gpus:
 if gpus:
     tf.config.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
 
-print(hvd.local_rank())
+mnist_images = np.load('data/mnist/mnist_x-%d.npy' % hvd.rank())
+mnist_labels = np.load('data/mnist/mnist_y-%d.npy' % hvd.rank())
 
-num_train_samples = 50000
+dataset = tf.data.Dataset.from_tensor_slices(
+    (tf.cast(mnist_images[..., tf.newaxis] / 255.0, tf.float32),
+             tf.cast(mnist_labels, tf.int64))
+)
+dataset = dataset.repeat().shuffle(10000).batch(128)
 
-x_train = np.empty((num_train_samples, 3, 32, 32), dtype='uint8')
-y_train = np.empty((num_train_samples,), dtype='uint8')
+mnist_model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, [3, 3], activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
+    tf.keras.layers.MaxPooling2D(pool_size=(2, 2)),
+    tf.keras.layers.Conv2D(64, [3, 3], activation='relu'),
+    tf.keras.layers.Dropout(0.25),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dropout(0.5),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
 
-for i in range(1, 6):
-    fpath = os.path.join('data/cifar10/', 'data_batch_' + str(i))
-    (x_train[(i - 1) * 10000:i * 10000, :, :, :],
-     y_train[(i - 1) * 10000:i * 10000]) = load_batch(fpath)
+loss = tf.losses.SparseCategoricalCrossentropy()
+opt = tf.optimizers.Adam(0.001 * hvd.size())
 
-train_labels = np.reshape(y_train, (len(y_train), 1))
-
-# Normalize pixel values to be between 0 and 1
-x_train = x_train.transpose(0, 2, 3, 1)
-train_images = x_train / 255.0
-
-train_dataset = tf.data.Dataset.from_tensor_slices((train_images, train_labels))
-train_dataset = train_dataset.shuffle(num_train_samples).batch(2048 // hvd.size())
-
-model = models.Sequential()
-model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-model.add(layers.Dropout(0.25))
-model.add(layers.Flatten())
-model.add(layers.Dense(128, activation='relu'))
-model.add(layers.Dropout(0.5))
-model.add(layers.Dense(10))
-
-model.summary()
-
-loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
-opt = tf.keras.optimizers.Adam(learning_rate=0.001 * hvd.size())
-
-checkpoint_dir = 'checkpoints/cifar10'
-checkpoint = tf.train.Checkpoint(model=model, optimizer=opt)
+checkpoint_dir = 'checkpoints/mnist'
+checkpoint = tf.train.Checkpoint(model=mnist_model, optimizer=opt)
 ckpt_manager = tf.train.CheckpointManager(checkpoint, checkpoint_dir, max_to_keep=10)
 
 @tf.function
 def training_step(images, labels, first_batch):
     with tf.GradientTape() as tape:
-        probs = model(images, training=True)
+        probs = mnist_model(images, training=True)
         loss_value = loss(labels, probs)
 
     # Horovod: add Horovod Distributed GradientTape.
     tape = hvd.DistributedGradientTape(tape)
 
-    grads = tape.gradient(loss_value, model.trainable_variables)
-    opt.apply_gradients(zip(grads, model.trainable_variables))
+    grads = tape.gradient(loss_value, mnist_model.trainable_variables)
+    opt.apply_gradients(zip(grads, mnist_model.trainable_variables))
 
     # Horovod: broadcast initial variable states from rank 0 to all other processes.
     # This is necessary to ensure consistent initialization of all workers when
@@ -101,14 +66,15 @@ def training_step(images, labels, first_batch):
     # Note: broadcast should be done after the first gradient step to ensure optimizer
     # initialization.
     if first_batch:
-        hvd.broadcast_variables(model.variables, root_rank=0)
+        hvd.broadcast_variables(mnist_model.variables, root_rank=0)
         hvd.broadcast_variables(opt.variables(), root_rank=0)
 
     return loss_value
 
 
 for i in range(10000):
-    for batch, (images, labels) in enumerate(train_dataset):
+    print('hvd.local_rank(): ', hvd.local_rank())
+    for batch, (images, labels) in enumerate(dataset.take(10000 // hvd.size())):
         loss_value = training_step(images, labels, batch == 0)
 
         if batch % 10 == 0 and hvd.local_rank() == 0:
