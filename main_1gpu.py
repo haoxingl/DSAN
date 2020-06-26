7712bb8f29fbe511bc5ec3f50176377d3ad9ef8a
hxstarklin <mb85410@umac.mo>
2020-06-26 21:47:58 +0800

    finalize es_epoch

--- main_1gpu.py
+++ main_1gpu.py
@@ -8,18 +8,18 @@ from utils.tools import ResultWriter
 
 parser = argparse.ArgumentParser(description='Hyperparameters')
 parser.add_argument('--dataset', default='taxi', help='taxi or bike or ctm')
-parser.add_argument('--gpu_ids', default='2, 3', help='indexes of gpus to use')
+parser.add_argument('--gpu_ids', default='0', help='indexes of gpus to use')
 parser.add_argument('--memory_growth', default=True)
-parser.add_argument('--index', default='SOTA_2GPUs_8000', help='indexes of model to be trained')
+parser.add_argument('--index', default='SOTA_1GPU', help='indexes of model to be trained')
 parser.add_argument('--test_name', default=None)
 parser.add_argument('--hyp', default=[None])
-parser.add_argument('--run_time', default=1)
+parser.add_argument('--run_time', default=3)
 parser.add_argument('--remove_old_files', default=True)
 parser.add_argument('--load_saved_data', default=True)
 parser.add_argument('--no_save', default=False)
 parser.add_argument('--test_model', default=None)
 parser.add_argument('--mixed_precision', default=False)
-parser.add_argument('--always_test', default=1)
+parser.add_argument('--always_test', default=None)
 
 """ Model hyperparameters """
 d_model = 64
@@ -36,13 +36,13 @@ weights_t = np.array([1 for _ in range(12)], dtype=np.float32)[:, np.newaxis]
 weights_f = np.array([1 for _ in range(2)], dtype=np.float32)[np.newaxis, :]
 weights = None
 parser.add_argument('--MAX_EPOCH', default=250)
-parser.add_argument('--BATCH_SIZE', default=256)
-parser.add_argument('--warmup_steps', default=8000)
+parser.add_argument('--BATCH_SIZE', default=64)
+parser.add_argument('--warmup_steps', default=64000)
 parser.add_argument('--verbose_train', default=1)
 parser.add_argument('--weights', default=weights)
 parser.add_argument('--es_patience', default=5)
-parser.add_argument('--es_threshold', default=0.00001)
-parser.add_argument('--es_epoch', default=120)
+parser.add_argument('--es_threshold', default=0.01)
+parser.add_argument('--es_epoch', default=40)
 parser.add_argument('--model_summary', default=True)
 
 """ Data hyperparameters """
