c1a0488556d16f36f6419fd833d11287c0ffdc39
hxstarklin <mb85410@umac.mo>
2020-06-27 08:28:47 +0800

    stop memory growth

--- main_1gpu.py
+++ main_1gpu.py
@@ -9,7 +9,7 @@ from utils.tools import ResultWriter
 parser = argparse.ArgumentParser(description='Hyperparameters')
 parser.add_argument('--dataset', default='taxi', help='taxi or bike or ctm')
 parser.add_argument('--gpu_ids', default='0', help='indexes of gpus to use')
-parser.add_argument('--memory_growth', default=True)
+parser.add_argument('--memory_growth', default=False)
 parser.add_argument('--index', default='SOTA_1GPU', help='indexes of model to be trained')
 parser.add_argument('--test_name', default=None)
 parser.add_argument('--hyp', default=[None])
